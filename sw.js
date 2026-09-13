ec72ea9a0b38f27b04843b204aa545ab2816116d
p6469sr8yw-blip <p6469sr8yw@privaterelay.appleid.com>
2026-09-13 14:42:43 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='kashf-sayara-v2'; self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg'])))); self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
